eaf684deed33a3a8a8662455ea3709ec51349aa6
Linux Lucas <lucaspereira.desenvolvedor@gmail.com>
2024-10-20 22:35:47 -0300

    Add user movement functionality in socket.io

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app)
 const serverPort = 3000
 const { Server } = require('socket.io')
 const io = new Server(server)
-const users = {}
+let users = {}
 
 app.use("/public", express.static("public", {}))
 
@@ -21,9 +21,15 @@ io.on('connection', (socket) => {
     console.log(`[${socket.id}] LOG: USER_DISCONNECTED`)
     users[socket.id] = undefined
   })
+
+  socket.on("ON_USER_MOVE", (newPosition) => {
+    const user = users[socket.id]
+    user.x = user.x + (newPosition.move.x || 0)
+    user.y = user.y + (newPosition.move.y || 0)
+    io.emit("ON_USERS_UPDATE", JSON.stringify(users))
+  })
 })
 
 server.listen(serverPort, () => {
   console.log(`listening on *: ${serverPort}`)
 })
-
