a2cc0151a475d8f0fd349cfdbe87ab8654d27266
Linux Lucas <lucaspereira.desenvolvedor@gmail.com>
2024-10-20 21:58:12 -0300

    Add basic server setup with Express and Socket.io

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const serverPort = 3000
+
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {})
+
+server.listen(serverPort, () => {
+  console.log(`listening on *: ${serverPort}`)
+})
+
